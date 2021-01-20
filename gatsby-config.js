61f4d1fe8ec1b55505a02d9c5cee62ee61f5ec42
Raghu Dinka Vijaykumar <raghu.dinka.vijaykumar@gmail.com>
2021-01-21 01:43:16 +0530

    Added study md to blog(phase1)

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,10 +14,81 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/study`,
-        name: `study`,
+        path: `${__dirname}/study/java/docs`,
+        name: `java`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/study/algorithms/docs`,
+        name: `algorithms`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/study/css/docs`,
+        name: `css`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/study/gradle/docs`,
+        name: `gradle`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/study/git/docs`,
+        name: `git`,
+        ignore: [`**/\*-orig.md`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/study/html/docs`,
+        name: `html`
+      },
+    },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `${__dirname}/study/computational-linguistics/docs`,
+          name: `computational-linguistics`,
+        },
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `${__dirname}/study/javascript/docs`,
+          name: `javascript`,
+        },
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `${__dirname}/study/react/docs`,
+          name: `react`,
+        },
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `${__dirname}/study/docker/docs`,
+          name: `docker`,
+        },
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `${__dirname}/study/kubernetes/docs`,
+          name: `kubernetes`,
+        },
+      },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
