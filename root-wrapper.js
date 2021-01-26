e375adc3aacc5a9924b6fc3071da38f94321658e
Raghu Dinka Vijaykumar <raghu.dinka.vijaykumar@gmail.com>
2021-01-26 20:49:24 +0530

    removed unnecessary h1 formatting

--- root-wrapper.js
+++ root-wrapper.js
@@ -3,7 +3,6 @@ import { Code } from "./src/components/formatter/Code";
 import React from "react";
 
 const components = {
-  h1: ({ children }) => <h2 style={{ color: "rebeccapurple" }}>{children}</h2>,
   "p.inlineCode": (props) => (
     <code style={{ backgroundColor: "lightgray" }} {...props} />
   ),
