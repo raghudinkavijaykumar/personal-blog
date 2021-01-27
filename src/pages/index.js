1bfa647246dc40fa1720af15535a86335a47df7c
raghudinkavijaykumar <raghu.dinka.vijaykumar@gmail.com>
2021-01-27 19:19:01 +0530

    Added AppToolBar to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { graphql, Link } from "gatsby";
 import { Layout } from "../components/Layout";
 import styled from "styled-components";
+import { Helmet } from "react-helmet";
+import { AppToolbar } from "../components/interface/app-toolbar"
 
 const IndexWrapper = styled.main``;
 const PostWrapper = styled.div``;
@@ -9,10 +11,18 @@ const PostWrapper = styled.div``;
 export default ({ data }) => {
   return (
     <>
+      <Helmet>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <meta name="author" content="Raghu Dinka Vijaykumar" ></meta>
+      </Helmet>
+      <AppToolbar />
       <Layout>
         <IndexWrapper>
           {data.allMdx.nodes.map(
-            ({ id, excerpt, frontmatter,  fields }) => (
+            ({ id, excerpt, frontmatter, fields }) => (
               <>
                 <PostWrapper key={id}>
                   <Link to={fields.slug}>
