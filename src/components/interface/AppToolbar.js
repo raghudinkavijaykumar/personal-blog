efc2b13b6f992532d8d93a713d32cdcbdd25703a
Raghu Dinka Vijaykumar <raghu.dinka.vijaykumar@gmail.com>
2021-01-31 14:30:10 +0530

    update gitignore

--- src/components/interface/AppToolbar.js
+++ src/components/interface/AppToolbar.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -25,7 +25,7 @@ export function AppToolbar() {
             <AppBar position="static" >
                 <Toolbar>
                     <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        {/*<MenuIcon />*/}
+                        <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
                         Blog
