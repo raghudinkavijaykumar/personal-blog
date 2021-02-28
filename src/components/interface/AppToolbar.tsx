748c475a4508bae7f5b65774e5e38fe2c018811c
raghudinkavijaykumar <raghu.dinka.vijaykumar@gmail.com>
2021-02-28 15:49:41 +0530

    removed menu icon

--- src/components/interface/AppToolbar.tsx
+++ src/components/interface/AppToolbar.tsx
@@ -2,7 +2,6 @@ import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 import { Link } from "gatsby";
 import React from "react";
@@ -38,17 +37,9 @@ export default function AppToolbar(props: { siteTitle: React.ReactNode }) {
   const classes = useStyles();
   return (
     <React.Fragment>
-      <Toolbar className={classes.toolbar}>
-        <IconButton
-          edge="start"
-          className={classes.toolbarMenuButton}
-          color="inherit"
-          aria-label="menu"
-        >
-          <MenuIcon />
-        </IconButton>
+      <Toolbar className={classes.toolbar} style={{ width: "100%" }}>
         <Typography
-          align="center"
+          align="left"
           variant="h5"
           noWrap
           className={classes.toolbarTitle}
